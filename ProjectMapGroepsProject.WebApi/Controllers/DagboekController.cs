ed2bd1e14fbaacf521bf6165eb63f5add5c3f162
TijnStoop <taj.stoop@student.avans.nl>
2025-03-31 13:13:20 +0200

    dagboek

---
+++ ProjectMapGroepsProject.WebApi/Controllers/DagboekController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProjectMapGroepsproject.WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ProjectMap.WebApi.Controllers
+{
+    [ApiController]
+    [Route("dagboeken")]
+    public class DagboekController : ControllerBase
+    {
+        private static readonly Dictionary<Guid, Dagboek> _dagboeken = new();
+        private readonly ILogger<DagboekController> _logger;
+
+        public DagboekController(ILogger<DagboekController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Dagboek>> GetAll()
+        {
+            return Ok(_dagboeken.Values);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Dagboek> Get(Guid id)
+        {
+            if (!_dagboeken.TryGetValue(id, out var dagboek))
+            {
+                return NotFound();
+            }
+            return Ok(dagboek);
+        }
+
+        [HttpPost]
+        public ActionResult<Dagboek> Create([FromBody] Dagboek dagboek)
+        {
+            var id = Guid.NewGuid();
+            dagboek.Id = id;
+            _dagboeken[id] = dagboek;
+            return CreatedAtAction(nameof(Get), new { id }, dagboek);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(Guid id, [FromBody] Dagboek updatedDagboek)
+        {
+            if (!_dagboeken.ContainsKey(id))
+            {
+                return NotFound();
+            }
+            updatedDagboek.Id = id;
+            _dagboeken[id] = updatedDagboek;
+            return Ok(updatedDagboek);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            if (!_dagboeken.Remove(id))
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
+    }
+}
+
+
+
---
+++ ProjectMapGroepsProject.WebApi/Repositories/DagboekRepository.cs
@@ -0,0 +1,71 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using ProjectMap.WebApi.Models;
+using ProjectMapGroepsproject.WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ProjectMap.WebApi.Repositories
+{
+    public class DagboekRepository
+    {
+        private readonly string _sqlConnectionString;
+
+        public DagboekRepository(string sqlConnectionString)
+        {
+            _sqlConnectionString = sqlConnectionString;
+        }
+
+        public async Task<Dagboek> InsertAsync(Dagboek dagboek)
+        {
+            using (var sqlConnection = new SqlConnection(_sqlConnectionString))
+            {
+                dagboek.Id = Guid.NewGuid();
+                await sqlConnection.ExecuteAsync(
+                    "INSERT INTO [Dagboek] (Id, DagboekBladzijde1, DagboekBladzijde2, DagboekBladzijde3, DagboekBladzijde4) " +
+                    "VALUES (@Id, @DagboekBladzijde1, @DagboekBladzijde2, @DagboekBladzijde3, @DagboekBladzijde4)", dagboek);
+                return dagboek;
+            }
+        }
+
+        public async Task<Dagboek?> ReadAsync(Guid id)
+        {
+            using (var sqlConnection = new SqlConnection(_sqlConnectionString))
+            {
+                return await sqlConnection.QuerySingleOrDefaultAsync<Dagboek>(
+                    "SELECT * FROM [Dagboek] WHERE Id = @Id", new { id });
+            }
+        }
+
+        public async Task<IEnumerable<Dagboek>> ReadAsync()
+        {
+            using (var sqlConnection = new SqlConnection(_sqlConnectionString))
+            {
+                return await sqlConnection.QueryAsync<Dagboek>("SELECT * FROM [Dagboek]");
+            }
+        }
+
+        public async Task UpdateAsync(Dagboek dagboek)
+        {
+            using (var sqlConnection = new SqlConnection(_sqlConnectionString))
+            {
+                await sqlConnection.ExecuteAsync(
+                    "UPDATE [Dagboek] SET " +
+                    "DagboekBladzijde1 = @DagboekBladzijde1, " +
+                    "DagboekBladzijde2 = @DagboekBladzijde2, " +
+                    "DagboekBladzijde3 = @DagboekBladzijde3, " +
+                    "DagboekBladzijde4 = @DagboekBladzijde4 " +
+                    "WHERE Id = @Id", dagboek);
+            }
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            using (var sqlConnection = new SqlConnection(_sqlConnectionString))
+            {
+                await sqlConnection.ExecuteAsync("DELETE FROM [Dagboek] WHERE Id = @Id", new { id });
+            }
+        }
+    }
+}
