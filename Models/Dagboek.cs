3b8ac0145ec7053e99f6b1638211e92506e7725a
TijnStoop <taj.stoop@student.avans.nl>
2025-04-01 11:14:39 +0200

    agenda

---
+++ Models/Dagboek.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectMapGroepsproject.WebApi.Models
+{
+    public class Dagboek
+    {
+        public Guid Id { get; set; }
+
+        [StringLength(250)]
+        public string? DagboekBladzijde1 { get; set; }
+
+        [StringLength(250)]
+        public string? DagboekBladzijde2 { get; set; }
+
+        [StringLength(250)]
+        public string? DagboekBladzijde3 { get; set; }
+
+        [StringLength(250)]
+        public string? DagboekBladzijde4 { get; set; }
+
+        public Guid UserId { get; set; }
+
+    }
+}
